32d897bad1c765ea337955ee574349e92e79e227
KhangNguyen3006 <111125627+KhangNguyen3006@users.noreply.github.com>
2023-07-28 00:57:23 +0700

    Dung khung layout TikTok 28/07/23 00:57 AM

---
+++ src/commponents/Layout/DefaultLayout/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "~/commponents/Layout/commponents/Header";
+import Sidebar from "./Sidebar";
+import styles from './DefaultLayout.module.scss'
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles)
+
+function DefaultLayout({ children }) {
+  return (
+    <div className={cx('wrapper')}>
+      <Header />
+      <div className={cx('container')}>
+        <Sidebar />
+        <div className={cx('content')}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
